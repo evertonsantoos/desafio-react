9be3bbd278e9ee0cd1f8a623ebfad93c6d3a9d1c
Everton <evertoncefal1@gmail.com>
2025-04-11 13:08:01 -0300

    structure

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Produtos from "./Components/Produtos";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+
+const App = () => {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Produtos />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/Produtos.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "./Produtos.module.css";
+
+const Produtos = () => {
+  return <h1>Produtos</h1>;
+};
+
+export default Produtos;
